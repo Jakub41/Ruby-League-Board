e82c853f384ff35c0b562bc8269523ea981869d1
JakubA3informatics <jl@a3informatics.com>
2018-03-04 15:46:36 +0100

    Added parse games sample text input

--- soccer_league_board.rb
+++ soccer_league_board.rb
@@ -1,3 +1,25 @@
 class SoccerLeagueBoard
+  # For debug the code
+  require "byebug"
+
+  def claculate_soccer_board
+
+  end
+
+  def parse_games_imput
+    games = []
+    File.open("sample-input.txt", "rb") do |file|
+      file.each_line do |line|
+        games << line.strip.plit('.').map {
+          |entry| [entry.delete(entry.split(' ').last).strip, entry.split(' ').last]
+        }
+      end
+      games
+    end
+  end
+
+  def calculate_games_results()
+
+  end
 
 end
