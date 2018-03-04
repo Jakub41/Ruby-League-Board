7c957e713cc87be07432fc9a7de9f9be2de7e98c
JakubA3informatics <jl@a3informatics.com>
2018-03-04 17:32:34 +0100

    Fix for a small bug and refactored ccode

--- soccer_league_board.rb
+++ soccer_league_board.rb
@@ -1,5 +1,4 @@
 class SoccerLeagueBoard
-  # For debug the code
   require 'byebug'
 
   # Get input file name from user until a valid file name is provided. Find the Soccer Team ranking and display it at console
@@ -7,9 +6,9 @@ class SoccerLeagueBoard
   # @return [Array] The final results which are displayed at console.
   def calculate_soccer_board
     while true
-      puts "Please enter input file name"
-      file_name = gets_chomp
-      break if file.exist?(file_name)
+      puts "Please enter input file name:"
+      file_name = gets.chomp
+      break if File.exist?(file_name)
       puts "ERROR!!! File name #{file_name} is incorrect"
     end
     games = parse_games_input(file_name)
@@ -20,23 +19,23 @@ class SoccerLeagueBoard
   # Read input file and perform ETL to setup data into pre-defined structure.
   # @param file_name [String] from user input on console.
   # @return [Array] the data from input file is converted into the expected format.
-  def parse_games_imput(file_name)
+  def parse_games_input(file_name)
     games = []
     File.open(file_name, "rb") do |file|
       file.each_line do |line|
-        games << line.strip.plit('.').map {
-          |entry| [entry.delete(entry.split(' ').last).strip, entry.split(' ').last]
-        }
+        games << line.strip.split(',').map {|entry| [entry.delete(entry.split(' ').last).strip, entry.split(' ').last]}
       end
       games
     end
   end
 
   # Get games information in a predefined format and calculate the score.
-  # A winner team will get +3 points, a loser team will get 0 points and when a match is draw each team will be awarded by 1 point.
+  # In this league, a draw (tie) is worth 1 point and a win is worth 3 points.
+  # A loss is worth 0 points. If two or more teams have the same number of points,
+  # they should have the same rank and be printed in alphabetical order
   # @param games [Array] games data into predefined format.
   # @return [Hash] the sorted results by number of points
-  def calculate_games_results(games)
+  def calculate_games_result(games)
     results = {}
     games.each do |game|
       if game.first.last > game.last.last
